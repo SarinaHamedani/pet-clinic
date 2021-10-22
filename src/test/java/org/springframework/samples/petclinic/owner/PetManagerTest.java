72e41125e21f3a405829fbd5360fd2a75b973ae6
SarinaHamedani <sarinahamedani3@gmail.com>
2021-10-22 17:18:28 +0330

    Add test fixture for PetManager

--- src/test/java/org/springframework/samples/petclinic/owner/PetManagerTest.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetManagerTest.java
@@ -1,11 +1,32 @@
 package org.springframework.samples.petclinic.owner;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.slf4j.Logger;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.samples.petclinic.utility.PetTimedCache;
 import org.springframework.samples.petclinic.utility.SimpleDI;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@SpringBootTest
 class PetManagerTest {
+	@MockBean
+	private PetTimedCache petTimedCache;
+	@MockBean
+	private OwnerRepository ownerRepository;
+	@MockBean
+	private Logger logger;
+	private PetManager petManager;
 
+	@BeforeEach
+	public void initialize() {
+		this.petManager = new PetManager(petTimedCache, ownerRepository, logger);
+	}
 
 }
