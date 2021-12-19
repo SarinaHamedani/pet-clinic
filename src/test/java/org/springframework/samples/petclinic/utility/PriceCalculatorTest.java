1d0f6f787be61a7e527c7e35b81a8a22fa222e3e
SarinaHmd <sarinahamedani3@gmail.com>
2021-12-19 15:43:05 +0330

    Add tests for discount cases

--- src/test/java/org/springframework/samples/petclinic/utility/PriceCalculatorTest.java
+++ src/test/java/org/springframework/samples/petclinic/utility/PriceCalculatorTest.java
@@ -2,6 +2,7 @@ package org.springframework.samples.petclinic.utility;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.samples.petclinic.owner.Pet;
+import org.springframework.samples.petclinic.visit.Visit;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -31,6 +32,24 @@ class PriceCalculatorTest {
 		when(pet.getBirthDate()).thenReturn(LocalDate.of(2021,7,9));
 		when(pet.getVisitsUntilAge(4)).thenReturn(new ArrayList<>());
 		assertEquals(16.8, PriceCalculator.calcPrice(List.of(pet), 10, 10), 0.1);
+	}
+
+	@Test
+	public void testInfantPetWithNoVisitsAndRecentDiscount() {
+		Pet pet = mock(Pet.class);
+		when(pet.getBirthDate()).thenReturn(LocalDate.of(2021,7,9));
+		when(pet.getVisitsUntilAge(0)).thenReturn(new ArrayList<>());
+		assertEquals(161.2, PriceCalculator.calcPrice(List.of(pet, pet, pet, pet, pet), 10, 10), 0.1);
+	}
+
+	@Test
+	public void testInfantPetWithNoVisitsAndOldVisitedDiscount() {
+		Pet pet = mock(Pet.class);
+		Visit visit = new Visit();
+		visit.setDate(LocalDate.of(2020,12,25));
+		when(pet.getBirthDate()).thenReturn(LocalDate.of(2021,7,9));
+		when(pet.getVisitsUntilAge(0)).thenReturn(List.of(visit));
+		assertEquals(325.6, PriceCalculator.calcPrice(List.of(pet, pet, pet, pet, pet), 10, 10), 0.1);
 	}
 
 }
