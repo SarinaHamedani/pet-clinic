d91031ee2954459f92dfa0573ddba1d9c11a2f50
SarinaHamedani <sarinahamedani3@gmail.com>
2021-12-20 20:47:45 +0330

    Modify PriceCalculator for 100% mutation coverage

--- src/test/java/org/springframework/samples/petclinic/utility/PriceCalculatorTest.java
+++ src/test/java/org/springframework/samples/petclinic/utility/PriceCalculatorTest.java
@@ -14,14 +14,22 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class PriceCalculatorTest {
-	LocalDate today, fiveYearsAgo;
+	LocalDate today, fiveYearsAgo, thisYear, lastYear;
 
 	@BeforeEach
 	public void setUp() {
 		today = LocalDate.now();
-		fiveYearsAgo = today.minusYears(5);
-		fiveYearsAgo = fiveYearsAgo.minusDays(20);
+		thisYear = LocalDate.now().minusDays(90);
+		lastYear = LocalDate.now().minusYears(1).minusDays(50);
+		fiveYearsAgo = today.minusYears(5).minusDays(20);
 	}
+
+	@Test
+	public void testInitialized() {
+		PriceCalculator p = new PriceCalculator();
+		assertNotNull(p);
+	}
+
 	@Test
 	public void testZeroPetsPriceCalculator() {
 		assertEquals(0, PriceCalculator.calcPrice(new ArrayList<>(), 10, 10));
@@ -30,24 +38,21 @@ class PriceCalculatorTest {
 	@Test
 	public void testMaturePetWithNoVisits() {
 		Pet pet = mock(Pet.class);
-		when(pet.getBirthDate()).thenReturn(LocalDate.of(2017,7,9));
-		when(pet.getVisitsUntilAge(4)).thenReturn(new ArrayList<>());
+		when(pet.getBirthDate()).thenReturn(fiveYearsAgo);
 		assertEquals(12, PriceCalculator.calcPrice(List.of(pet), 10, 10));
 	}
 
 	@Test
 	public void testInfantPetWithNoVisits() {
 		Pet pet = mock(Pet.class);
-		when(pet.getBirthDate()).thenReturn(LocalDate.of(2021,7,9));
-		when(pet.getVisitsUntilAge(4)).thenReturn(new ArrayList<>());
+		when(pet.getBirthDate()).thenReturn(thisYear);
 		assertEquals(16.8, PriceCalculator.calcPrice(List.of(pet), 10, 10), 0.1);
 	}
 
 	@Test
 	public void testInfantPetWithNoVisitsAndRecentDiscount() {
 		Pet pet = mock(Pet.class);
-		when(pet.getBirthDate()).thenReturn(LocalDate.of(2021,7,9));
-		when(pet.getVisitsUntilAge(0)).thenReturn(new ArrayList<>());
+		when(pet.getBirthDate()).thenReturn(thisYear);
 		assertEquals(161.2, PriceCalculator.calcPrice(List.of(pet, pet, pet, pet, pet), 10, 10), 0.1);
 	}
 
@@ -56,16 +61,15 @@ class PriceCalculatorTest {
 		Pet pet = mock(Pet.class);
 		Visit visit = new Visit();
 		visit.setDate(LocalDate.of(2020,12,25));
-		when(pet.getBirthDate()).thenReturn(LocalDate.of(2021,7,9));
-		when(pet.getVisitsUntilAge(0)).thenReturn(List.of(visit));
-		assertEquals(325.6, PriceCalculator.calcPrice(List.of(pet, pet, pet, pet, pet), 10, 10), 0.1);
+		when(pet.getBirthDate()).thenReturn(thisYear);
+		when(pet.getVisitsUntilAge(0)).thenReturn(List.of(visit, visit));
+		assertEquals(402.8, PriceCalculator.calcPrice(List.of(pet, pet, pet, pet, pet), 10, 10), 0.1);
 	}
 
 	@Test
 	public void testMultipleMaturePetsWithNoVisits() {
 		Pet pet = mock(Pet.class);
-		when(pet.getBirthDate()).thenReturn(LocalDate.of(2017,7,9));
-		when(pet.getVisitsUntilAge(4)).thenReturn(new ArrayList<>());
+		when(pet.getBirthDate()).thenReturn(fiveYearsAgo);
 		assertEquals(36, PriceCalculator.calcPrice(List.of(pet, pet, pet), 10, 10));
 	}
 
@@ -73,7 +77,6 @@ class PriceCalculatorTest {
 	public void testMultipleInfantPetsWithNoVisits() {
 		Pet pet = mock(Pet.class);
 		when(pet.getBirthDate()).thenReturn(LocalDate.of(2021,7,9));
-		when(pet.getVisitsUntilAge(4)).thenReturn(new ArrayList<>());
 		assertEquals(50.4, PriceCalculator.calcPrice(List.of(pet, pet, pet), 10, 10), 0.1);
 	}
 
@@ -81,7 +84,6 @@ class PriceCalculatorTest {
 	public void testMultipleTwoYearOldPetsWithNoVisits() {
 		Pet pet = mock(Pet.class);
 		when(pet.getBirthDate()).thenReturn(LocalDate.of(2019,7,9));
-		when(pet.getVisitsUntilAge(2)).thenReturn(new ArrayList<>());
 		assertEquals(16.8, PriceCalculator.calcPrice(List.of(pet), 10, 10), 0.1);
 	}
 
@@ -89,33 +91,26 @@ class PriceCalculatorTest {
 	public void testMultipleDifferentAgesPetsWithNoVisits() {
 		Pet infant = mock(Pet.class);
 		Pet mature = mock(Pet.class);
-		when(infant.getBirthDate()).thenReturn(LocalDate.of(2021,7,9));
-		when(infant.getVisitsUntilAge(4)).thenReturn(new ArrayList<>());
-		when(mature.getBirthDate()).thenReturn(LocalDate.of(2017,7,9));
-		when(mature.getVisitsUntilAge(4)).thenReturn(new ArrayList<>());
+		when(infant.getBirthDate()).thenReturn(thisYear);
+		when(mature.getBirthDate()).thenReturn(fiveYearsAgo);
 		assertEquals(45.6, PriceCalculator.calcPrice(List.of(infant, mature, infant), 10, 10), 0.1);
 	}
 
 	@Test
 	public void testMaturePetWithNoVisitsAndRecentDiscount() {
 		Pet pet = mock(Pet.class);
-		when(pet.getBirthDate()).thenReturn(LocalDate.of(2016,10,9));
-		when(pet.getVisitsUntilAge(5)).thenReturn(new ArrayList<>());
+		when(pet.getBirthDate()).thenReturn(fiveYearsAgo);
 		assertEquals(238, PriceCalculator.calcPrice(List.of(pet, pet, pet, pet, pet, pet, pet, pet, pet, pet), 10, 10));
 	}
 
 	@Test
 	public void testMultipleMaturePetWithOldVisitedDiscount() {
 		Pet pet = mock(Pet.class);
-		Visit visit1 = new Visit();
-		Visit visit2 = new Visit();
-		LocalDate today = LocalDate.now();
-		LocalDate past = today.minusYears(5);
-		visit1.setDate(LocalDate.of(2020,12,25));
-		visit2.setDate(LocalDate.of(2020,12,25));
-		when(pet.getBirthDate()).thenReturn(past);
-		when(pet.getVisitsUntilAge(5)).thenReturn(List.of(visit1, visit2));
-		assertEquals(602, PriceCalculator.calcPrice(List.of(pet, pet, pet, pet, pet, pet, pet, pet, pet, pet), 10, 10));
+		Visit visit = new Visit();
+		visit.setDate(lastYear);
+		when(pet.getBirthDate()).thenReturn(fiveYearsAgo);
+		when(pet.getVisitsUntilAge(5)).thenReturn(List.of(visit, visit));
+		assertEquals(720, PriceCalculator.calcPrice(List.of(pet, pet, pet, pet, pet, pet, pet, pet, pet, pet), 10, 10));
 	}
 
 	@Test
@@ -136,13 +131,13 @@ class PriceCalculatorTest {
 		Pet pet2 = mock(Pet.class);
 		Visit visit1 = new Visit();
 		Visit visit2 = new Visit();
-		visit1.setDate(LocalDate.of(2020,12,25));
-		visit2.setDate(LocalDate.of(2020,12,25));
-		when(pet1.getBirthDate()).thenReturn(LocalDate.of(2016,7,9));
+		visit1.setDate(lastYear);
+		visit2.setDate(thisYear);
+		when(pet1.getBirthDate()).thenReturn(fiveYearsAgo);
 		when(pet1.getVisitsUntilAge(5)).thenReturn(List.of(visit1, visit2));
-		when(pet2.getBirthDate()).thenReturn(LocalDate.of(2021,7,9));
+		when(pet2.getBirthDate()).thenReturn(thisYear);
 		when(pet2.getVisitsUntilAge(0)).thenReturn(List.of(visit1));
-		assertEquals(185192, PriceCalculator.calcPrice(List.of(pet1, pet2, pet2, pet1, pet2, pet2, pet1, pet2, pet1, pet1), 10, 10));
+		assertEquals(3158.8, PriceCalculator.calcPrice(List.of(pet1, pet2, pet2, pet1, pet2, pet2, pet1, pet2, pet1, pet1), 10, 10), 0.1);
 	}
 
 }
